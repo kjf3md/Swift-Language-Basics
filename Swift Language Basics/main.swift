2c7f379e7822d008c5df55500c08003647525a25
Kevin Fritz <kevinfritz@Kevins-MacBook-Pro.local>
2017-09-05 20:20:12 -0500

    First commit

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Kevin Fritz on 9/5/17.
+//  Copyright © 2017 Kevin Fritz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
